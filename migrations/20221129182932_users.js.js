289bbadd6f2f0fff0496cf0d12e8d164a071edca
evanlu1134 <evanlu1134@gmail.com>
2022-11-29 14:19:33 -0500

    added additional missing column name

--- migrations/20221129182932_users.js.js
+++ migrations/20221129182932_users.js.js
@@ -5,6 +5,7 @@
  exports.up = function(knex) {
     return knex.schema.createTable("users", table =>{
       table.increments('user_id').primary();
+      table.string("username").notNullable;
       table.string("first_name").notNullable;
       table.string("last_name").notNullable;
       table.string("email").notNullable;
