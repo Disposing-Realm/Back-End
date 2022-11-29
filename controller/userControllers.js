cbde7248b73684aabe1e21360e4a94bbd32c62db
Delvin Reyes <DelvinReyes.95@Gmail.com>
2022-11-29 13:59:37 -0500

    userFolders, app.js, auth.js

---
+++ controller/userControllers.js
@@ -0,0 +1,120 @@
+const Users = require("../model/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+// const databasePool = require("../db");
+
+const allUsers = async (req, res) => {
+  const users = await Users.totalUsers();
+  res.status(200).send(users);
+};
+
+const getUserById = async (req, res) => {
+  const userId = req.id;
+  const user = await Users.singleUser(userId);
+
+  if (user) {
+    res.status(200).send(user);
+  } else {
+    res.status(404).send("user not found");
+  }
+};
+
+const validateInputs = (email, password) => {
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (!emailRegex.test(email.toLowerCase())) return false;
+  if (password.length < 5) return false;
+  return true;
+};
+
+const userRegister = async (req, res) => {
+  try {
+    const {
+      email,
+      password,
+      first_name,
+      last_name,
+    } = req.body;
+    if (!validateInputs(email, password)) {
+      throw Error("Invalid Credentials.");
+    }
+
+    const saltRounds = 5;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const newUserInfo = {
+      first_name,
+      last_name,
+      email,
+      password: hashedPassword,
+    };
+    console.log(newUserInfo);
+    Users.createUser(newUserInfo);
+    const token = jwt.sign({ email: email }, process.env.AUTH_KEY);
+    res.cookie("token", token).sendStatus(200);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await Users.getByEmail(email);
+
+    if (!user) {
+      return res.status(401).send("User Does Not Exist.");
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    console.log(user.email);
+
+    if (isValidPassword) {
+      const token = jwt.sign({ email: user.email }, process.env.AUTH_KEY);
+      res.cookie("token", token).status(200).send(JSON.stringify(user));
+    }
+  } catch (err) {
+    res.status(500).send("An error has occured");
+    console.log(err);
+  }
+};
+
+const logout = (req, res) => {
+  res.clearCookie("token").sendStatus(200);
+};
+
+const userDelete = async (req, res) => {
+  const userId = req.id;
+  const user = await Users.deleteUser(userId);
+  if (user) {
+    res.status(200).send(user);
+  } else {
+    res.status(404).send("Deletion Unsuccessful");
+  }
+};
+
+const userUpdate = async (req, res) => {
+  const userId = req.id;
+  const { first_name, last_name } = req.body;
+
+  const user = await Users.updateUser({
+    id: userId,
+    first_name,
+    last_name,
+  });
+
+  if (user) {
+    res.status(200).send("Information successfully updated");
+  } else {
+    res.status(404).send();
+  }
+};
+
+module.exports = {
+  allUsers,
+  login,
+  logout,
+  getUserById,
+  userRegister,
+  userDelete,
+  userUpdate,
+};
---
+++ route/userRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const userController = require("../controller/userController");
+const authenticate = require("../auth");
+const router = express.Router();
+
+router.get("/", userController.totalUsers);
+router.post("/login", userController.login);
+router.get("/logout", userController.logout);
+
+router.get("/getById", authenticate, userController.getUserById);
+router.post("/", userController.userRegister);
+// router.delete("/:id", authenticate, userController.deleteUser);
+router.put("/", authenticate, userController.userUpdate);
+
+router.all("*", (req, res) => {
+  res.send("This user route does not exist");
+});
+
+module.exports = router;
