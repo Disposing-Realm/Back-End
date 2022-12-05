1bb968f7cc52ab774dc9595b0756a08ea40b19da
clopez11224 <christopherlopez000@gmail.com>
2022-12-05 04:05:44 -0500

    small fixes

--- controller/postsControllers.js
+++ controller/postsControllers.js
@@ -12,23 +12,23 @@ const getPost = async (req, res) => {
 };
 
 const createPost = async (req, res) => {
-  if (!req.info) {
-    res.status(400).send("Info not found in request")
-  }
+  // if (!req.info) {
+  //   res.status(400).send("Info not found in request")
+  // }
  
-  if (!req.info.post_description) {
-    res.status(400).send("Description not found in request")
-  }
+  // if (!req.info.post_description) {
+  //   res.status(400).send("Description not found in request")
+  // }
 
-  if (!req.info.post_image) {
-    res.status(400).send("Image not found in request")
-  }
-
-  await Post.createPost(req.info);
+  // if (!req.info.post_image) {
+  //   res.status(400).send("Image not found in request")
+  // }
+ 
+  await Post.createPostModel(req.body);
   res.status(200).send("Post created successfully")
 }
-
-
+ 
+ 
 const addPost = async (req, res) => {
   const { post_description, user_id } = req.body
   console.log(req.body)
@@ -43,3 +43,4 @@ module.exports = {
   createPost,
   addPost
 }
+  
--- model/postsModel.js
+++ model/postsModel.js
@@ -14,13 +14,13 @@ class Post {
     const disposingResults = await pool.query(database, [id]);
     return { ...disposingdb2.rows[0], ...disposingResults.rows[0] };
   }
-  static async createPost(info) {
+  static async createPostModel(info) {
     const db1 =
-      "INSERT INTO posts (post_description, post_image, post_date, update_date, likes, dislikes) VALUES ($1, $2, $3, $4, $5, $6) RETURNING*";
+      "INSERT INTO posts (post_description, post_image) VALUES ($1, $2) RETURNING *";
     const disposingResults = await pool.query(db1, [
       info.post_description,
       info.post_image,
-    ]);
+    ]); 
   }
 }
 
