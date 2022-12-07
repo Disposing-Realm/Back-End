c9d69bd5b22cccb0652ded57076c7c9bdae0385c
Delvin Reyes <DelvinReyes.95@Gmail.com>
2022-12-07 11:06:37 -0500

    modified postsControls

--- controller/postsControllers.js
+++ controller/postsControllers.js
@@ -12,15 +12,15 @@ const getPost = async (req, res) => {
 };
 
 const createPost = async (req, res) => {
-  if (!req.info) {
+  if (!req.body) {
     res.status(400).send("Info not found in request")
   }
  
-  if (!req.info.post_description) {
+  if (!req.body.post_description) {
     res.status(400).send("Description not found in request")
   }
 
-  if (!req.info.post_image) {
+  if (!req.body.post_image) {
     res.status(400).send("Image not found in request")
   }
  
